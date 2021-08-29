82f8ad1a1527fdf67db47e32866a4a91f4820ec6
Sandeep Kumar Shah <sandeep2016shah@gmail.com>
2021-08-29 21:20:37 +0530

    Done with Responsive

--- client/src/components/Extra.js
+++ client/src/components/Extra.js
@@ -22,11 +22,17 @@ const Extra = (props) => {
     const [search, setSearch] = useState("");
     const [chatroom, setChatroom] = useState(null);
     const [ to, setTo] = useState(null);
+    const [ showFriends, setShowFriends] = useState(true);
+    const [ showSearch, setShowSearch ] = useState(true);
      
     
    useEffect(() => {
         props.dispatch(show_users());
         props.dispatch(show_friends());
+        if(window.innerWidth<=600){
+            setShowFriends(false);
+            setShowSearch(false);
+        }
    },[search])
 
    const fuse = new Fuse(props.friends.users, {
@@ -46,6 +52,7 @@ const Extra = (props) => {
         console.log("fetching the messages from the DB!", id, to);
         props.dispatch(privateMessage(to));
         handleConnect(name,id, props.dispatch, from, to );
+        setShowFriends(false);
     }
 
     const sendMessage = (from) => {
@@ -93,6 +100,10 @@ const Extra = (props) => {
         }
     }
 
+    const handleshowFriends = () => {
+        setShowFriends(!showFriends);
+    }
+
         console.log("new props", props);
         //console.log("State Info", this.state);
         const { friends, allusers } = props.friends;
@@ -127,6 +138,27 @@ const Extra = (props) => {
                 </div>
                 <div className="message_container">
                     <p className="message_container_p">{name}</p>
+                    <div className="smallScreen">
+                        <div className="showFriends" onClick={ () => handleshowFriends() }><p>Friends</p></div> 
+                        {
+                            showFriends && 
+                            <div className="friends">
+                            {
+                                showFriends &&
+                                friends.map((friend,index) => <Friends from={_id} dispatch={props.dispatch} clickHandle={clickHandle}  key={friend._id} name={friend.name} to={friend._id}/>)
+                            }
+                        </div>
+                        }
+                        <div className="showSearch">
+                            <input type="text" value={search} onChange={ (e) => handleName(e) } placeholder="Search Friend" />
+                        </div> 
+                        { 
+                            characterResults.length>0 &&  
+                            <div className="searched">{
+                                characterResults.map((search) => <Searched key={search._id} handleAddFriend={handleAddFriend} name={search.name} id={search._id} clickHandle={clickHandle}/>)
+                            }</div>
+                        }
+                    </div>
                     <div>
                         <div className="message">
                             { 
